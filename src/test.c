211d07259809a8d81be2d8a6d123c335810e69ad
Maurice <mauricetrautner@gmail.com>
2023-07-26 16:50:54 +0200

    added testfile and headers to init a mvp working program that uses mlx

--- src/test.c
+++ src/test.c
@@ -1,7 +1,18 @@
 #include <unistd.h>
+#include "../inc/cub3d.h"
 
 int main(void)
 {
-	write(1, "test", 5);
+	void			*mlx_ptr;
+	void			*win_ptr;
+	double			width;
+	double			height;
+	char			*title = "cubed";
+
+	mlx_ptr = mlx_init();
+	width = 1200;
+	height = (double)round(width / 1.77);
+	win_ptr = mlx_new_window(mlx_ptr, width, height, title);
+	mlx_loop(mlx_ptr);
 	return (0);
 }
