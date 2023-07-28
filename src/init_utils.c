548a874bd372667d85f73d8a2ce5f8596c828e30
mtrautne <mtrautne@student.42wolfsburg.de>
2023-07-28 10:29:06 +0200

    added testmap

--- src/init_utils.c
+++ src/init_utils.c
@@ -6,7 +6,7 @@
 /*   By: mtrautne <mtrautne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 23:51:38 by mtrautne          #+#    #+#             */
-/*   Updated: 2023/07/28 00:58:33 by mtrautne         ###   ########.fr       */
+/*   Updated: 2023/07/28 09:59:07 by mtrautne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,20 @@
 int	mapfile_to_arr(t_vars *vrs)
 {
 	int bytes_read;
-	int	x;
-	int	y;
+	int	grid_x;
+	int	grid_y;
 
 	bytes_read = read(vrs->mapfile_fd, vrs->map_str, 2048);
 	if (bytes_read == -1)
 		return (err_msg("failed to read mapfile"));
 	printf("%s\n", vrs->map_str);
-	x = y = 0;
-	vrs->map = 
-	while(x < vrs->map_width && y < vrs->map_height)
+	grid_x = grid_y = 0;
+	vrs->map = malloc(sizeof(int *) * vrs->map_height);
+	if (!vrs->map)
+		return (err_msg("failed to malloc map"));
+	while(grid_x < vrs->map_width && grid_y < vrs->map_height)
 	{
+		vrs->map[y]
 		if (x == vrs->map_width - 1)
 		{
 			x = 0;
