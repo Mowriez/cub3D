ff13e8f49e2f1ce4fcf6e0edd61fc1aee31bf7fc
mtrautne <mtrautne@student.42wolfsburg.de>
2023-08-08 00:30:31 +0200

    beautiful new textures

--- src/init_textures.c
+++ src/init_textures.c
@@ -6,7 +6,7 @@
 /*   By: mtrautne <mtrautne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/05 21:51:40 by mtrautne          #+#    #+#             */
-/*   Updated: 2023/08/07 11:05:16 by mtrautne         ###   ########.fr       */
+/*   Updated: 2023/08/08 00:29:32 by mtrautne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void	init_textures(t_vars *vrs)
 void	init_texture_n(t_vars *vrs)
 {
 	vrs->tx_n = malloc(sizeof(t_tex));
-	vrs->tx_n->path_to_file = "./tex/brick_north.xpm";
+	vrs->tx_n->path_to_file = "./tex/texture_from_hell.xpm";
 	vrs->tx_n->img_ptr = mlx_xpm_file_to_image(vrs->mlx_ptr,
 			vrs->tx_n->path_to_file, &vrs->tx_n->width, &vrs->tx_n->height);
 	vrs->tx_n->img_data_addr = mlx_get_data_addr(vrs->tx_n->img_ptr,
@@ -33,7 +33,7 @@ void	init_texture_n(t_vars *vrs)
 void	init_texture_s(t_vars *vrs)
 {
 	vrs->tx_s = malloc(sizeof(t_tex));
-	vrs->tx_s->path_to_file = "./tex/brick_south.xpm";
+	vrs->tx_s->path_to_file = "./tex/texture_from_hell.xpm";
 	vrs->tx_s->img_ptr = mlx_xpm_file_to_image(vrs->mlx_ptr,
 			vrs->tx_s->path_to_file, &vrs->tx_s->width, &vrs->tx_s->height);
 	vrs->tx_s->img_data_addr = mlx_get_data_addr(vrs->tx_s->img_ptr,
@@ -43,7 +43,7 @@ void	init_texture_s(t_vars *vrs)
 void	init_texture_w(t_vars *vrs)
 {
 	vrs->tx_w = malloc(sizeof(t_tex));
-	vrs->tx_w->path_to_file = "./tex/brick_west.xpm";
+	vrs->tx_w->path_to_file = "./tex/texture_from_hell.xpm";
 	vrs->tx_w->img_ptr = mlx_xpm_file_to_image(vrs->mlx_ptr,
 			vrs->tx_w->path_to_file, &vrs->tx_w->width, &vrs->tx_w->height);
 	vrs->tx_w->img_data_addr = mlx_get_data_addr(vrs->tx_w->img_ptr,
@@ -53,7 +53,7 @@ void	init_texture_w(t_vars *vrs)
 void	init_texture_e(t_vars *vrs)
 {
 	vrs->tx_e = malloc(sizeof(t_tex));
-	vrs->tx_e->path_to_file = "./tex/brick_east.xpm";
+	vrs->tx_e->path_to_file = "./tex/texture_from_hell.xpm";
 	vrs->tx_e->img_ptr = mlx_xpm_file_to_image(vrs->mlx_ptr,
 			vrs->tx_e->path_to_file, &vrs->tx_e->width, &vrs->tx_e->height);
 	vrs->tx_e->img_data_addr = mlx_get_data_addr(vrs->tx_e->img_ptr,
